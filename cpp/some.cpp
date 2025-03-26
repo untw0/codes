a47baf16a5ca79ed88eac273138431d450f93b82
Maiquel Paiva <153939765+untw0@users.noreply.github.com>
2025-03-26 02:49:53 -0300

    Create some.cpp

---
+++ cpp/some.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int a, b;
+    
+    std::cout << "Enter two numbers: ";
+    std::cin >> a >> b;
+    
+    std::cout << "Sum: " << (a + b) << std::endl;
+    return 0;
+}
